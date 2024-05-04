a4ee0f79f8c933ae74cfc54408fd6f595eb10132
Ali Akber <94167678+aliakber841@users.noreply.github.com>
2024-05-04 09:34:19 +0500

    fibonacci-series.asm

--- fibonacci-series.asm
+++ fibonacci-series.asm
@@ -1,4 +1,4 @@
-  org 100h
+    org 100h
   
   TITLE DISPLAY SUM OF IST 7 FIBONACCI SERIES
   main PROC 
@@ -7,18 +7,18 @@
     MOV AX,@data
     MOV DS,AX
      
-    MOV BX,0
-    MOV DX,1
+    MOV BX,0  ;BX=0
+    MOV DX,1  ;DX=1
     
     
-    MOV CX,7
+    MOV CX,7  ;LOOP 7 TIMES
     
     
     FIB:
-    MOV AX,DX
-    ADD BX,DX
-    MOV DX,BX
-    MOV BX,AX
+    MOV AX,DX  ;AX=1 (storing dx value in a register temporarily)
+    ADD BX,DX  ;BX=1 (adding bx,dx)
+    MOV DX,BX  ;DX=1 (result of addition will go to dx)
+    MOV BX,AX  ;BX=1 (bx value will be dx previous value which we stored in ax)
     loop FIB: 
    
       
